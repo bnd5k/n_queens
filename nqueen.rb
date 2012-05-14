60a3849fa6e191f9eccc4f48e2cdf4b3f25252d1
Ben Downey <BD@BenDowney.net>
2012-05-14 14:20:22 -0500

    cleaned up and annotated code

--- nqueen.rb
+++ nqueen.rb
@@ -2,40 +2,51 @@ class Nqueen
 
   def initialize(n)
     if n.even?
-      first_queen_in_array = (n/2)+1
+      @first_queen_in_array = (n/2)+1 
       @sm_intv = n/2
       @lg_intv = (n+1) + @sm_intv
     else
-      first_queen_in_array = (n+1)/2
+      @first_queen_in_array = (n+1)/2
       @sm_intv = (n+1)/2
       @lg_intv = (n) + @sm_intv
     end
-    @first_queen_in_array = first_queen_in_array.to_i
   end
 
   def determine_intervals_between_queens(n)
+    #my algo consists of alternating invertals, one large, on small. This method generates that interval sequence as an array
     @interval_sequence = [@first_queen_in_array]
-    (n/1).times do
+    (n-1).times do
       @interval_sequence << @sm_intv
       @interval_sequence << @lg_intv
     end
-    #puts @interval_sequence #=> [3,2, 7,2]
   end
 
   def translate_intervals_into_board_positions(n)
-    # takes the interval sequence and turns it into an array of where the queens should be positioned. 
-    # Note: the tranlsated interval sequence ultimately yields a list of the positions that are prior to where a queen will go.
-    # That's because the construct_solution_board method relies on the position alread being included in the array
+    # turns the interval sequence into a list of the positions--not of queens, but of positions prior to a quee..
+    # (Why the position prior? That's because the construct_solution_array  method relies on the position already being included in the array.)
     @queen_array = []
     sum = 0
     @interval_sequence.each do |item|
       sum = sum + item  
       @queen_array << sum-1 
     end
-    #puts @queen_array #=> [3,5,12,14]
+  end
+
+  def construct_solution_array(n)
+    # generates array that contains the solution.  Q's representing where the queen's are positioned.
+    @board = ['*']
+    while @board.size < (n*n)
+      if @queen_array.include?(@board.size)
+        new_item = 'Q'
+      else
+        new_item = '*'
+      end
+      @board << new_item
+    end
   end
 
   def initial_state_of_chessboard(n)
+    #generates how the starting position of the chess board would look.
     puts "\n Here is the initial state of the chess board.\n"
     n.times  {print 'Q'}
     print " \n"
@@ -46,20 +57,11 @@ class Nqueen
       print "  \n"
     end
   end
-
-  def construct_solution_board(n)
+  
+  def terminal_state_of_chessboard(n)
+    # chops the solution array into sub-arrays of n size, then turns them into a string by joining them
     puts "\n Here is the solution. \n"
-    board = ['*']
-    while board.size < (n*n)
-      if @queen_array.include?(board.size)
-        new_item = 'Q'
-      else
-        new_item = '*'
-      end
-      board << new_item
-    end
-    my_board = board.each_slice(n).to_a
-    #print board
+    my_board = @board.each_slice(n).to_a
     my_board.each do |sub_array|
       nub = sub_array.join
       puts nub
@@ -79,6 +81,7 @@ end
 my_queen = Nqueen.new(n)
 my_queen.determine_intervals_between_queens(n)
 my_queen.translate_intervals_into_board_positions(n)
+my_queen.construct_solution_array(n)
 my_queen.initial_state_of_chessboard(n)
-my_queen.construct_solution_board(n)
+my_queen.terminal_state_of_chessboard(n)
 
